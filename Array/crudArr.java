d421678fe669c77a0efff8e411c80c8db25bfac8
abhishek kumar sharma <akshaarma202121@gmail.com>
2025-10-04 22:43:30 +0530

    CRUD OPERATION

---
+++ Array/crudArr.java
@@ -0,0 +1,18 @@
+public class crudArr {
+    public static void  update(int marks[]){
+        for(int i=0;i<marks.length;i++){
+            marks[i]=marks[i]+1;
+        }
+
+    }
+    public static void main(String[] args) {
+        int marks[]={98,74,95};
+
+        update(marks);
+        // print our marks in loop way 
+        for(int i=0;i<marks.length;i++){
+            System.out.println(marks[i]+" ");
+        }
+        System.out.println();
+    }
+}
