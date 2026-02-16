61050d27465c3a553e729c995d473959f465e158
abhishek kumar sharma <akshaarma202121@gmail.com>
2026-02-16 09:25:21 +0530

    WAP find MAX

--- 2_Function&Methord/max3numbers.java
+++ 2_Function&Methord/max3numbers.java
@@ -1,9 +1,26 @@
 
 public class max3numbers {
-    // max of       
+
+    // max of 3 numbers
+    public static int max(int a, int b, int c ){
+         if (a>=b && a>=c){
+            return a;
+         }else if (b>c){
+            return b;
+
+         }else{
+            return c;
+         }
+         
+        
 
 
-    // method with return value
+    
+
+    }
+
+
+    // method with return value sum of a &b
     public static int add(int a, int b) {
         return a+b;
     }
@@ -15,8 +32,11 @@ public class max3numbers {
     public static void main(String[] args) {
         //sayHello();
 
-        int result =   add(4,5);
-        System.out.println("sum of 2 number is "+result);
+        //int result =   add(4,5);
+
+        int result2=max(5, 6, 9);
+
+        System.out.println("MAX  of 3 number is "+result2);
 
     }
 }
