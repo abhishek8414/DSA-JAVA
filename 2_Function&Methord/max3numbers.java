d296d958513bdb1654e2712b86cbf4e06f7eb1c0
abhishek kumar sharma <akshaarma202121@gmail.com>
2026-02-16 12:54:07 +0530

    isma excel sheet ko modify kiye hai or code me kuch vher char kiye hai

--- 2_Function&Methord/max3numbers.java
+++ 2_Function&Methord/max3numbers.java
@@ -2,41 +2,35 @@
 public class max3numbers {
 
     // max of 3 numbers
-    public static int max(int a, int b, int c ){
-         if (a>=b && a>=c){
+    public static int max(int a, int b, int c) {
+        if (a >= b && a >= c) {
             return a;
-         }else if (b>c){
+        } else if (b > c) {
             return b;
 
-         }else{
+        } else {
             return c;
-         }
-         
-        
-
-
-    
-
+        }
     }
-
 
     // method with return value sum of a &b
     public static int add(int a, int b) {
-        return a+b;
+        return a + b;
     }
     // simple java methord
 
-    /*public static void sayHello(){
+    public static void sayHello() {
         System.out.println("Hello World !");
-    }*/
+    }
+
+    // main class 
     public static void main(String[] args) {
         //sayHello();
 
         //int result =   add(4,5);
+        int result2 = max(5, 6, 9);
 
-        int result2=max(5, 6, 9);
-
-        System.out.println("MAX  of 3 number is "+result2);
+        System.out.println("MAX  of 3 number is " + result2);
 
     }
 }
