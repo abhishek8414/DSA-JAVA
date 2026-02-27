ac8efea94e50731d0596de2f7b7b1c4e2d351e9d
abhishek kumar sharma <akshaarma202121@gmail.com>
2026-02-27 22:18:00 +0530

    Bassic of BitManuplation

--- 18_BitManuplation/bassicBitManup.java
+++ 18_BitManuplation/bassicBitManup.java
@@ -1,5 +1,14 @@
 
+import java.util.logging.XMLFormatter;
+
+
 
 public class bassicBitManup {
-    
+    //isme mai sikhne hja rha hu bit manuplation ke technhque anr kuch problen with solution 
+    // or kuch ka karne jaung ato kam karunga 
+    // Bitmanyplation me 4 type ke operator hote hai 
+    1= or (|);
+    2= Xor (^);
+    3= and (&);
+    4=  
 }
