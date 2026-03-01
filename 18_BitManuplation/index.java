878ef8c10f5f61cb37f6a3833d3dd37fc2a9786c
abhishek kumar sharma <akshaarma202121@gmail.com>
2026-03-01 22:57:54 +0530

    Swap of two number without using third variable

--- 18_BitManuplation/index.java
+++ 18_BitManuplation/index.java
@@ -2,6 +2,9 @@
 
 public class index {
     public static void main(String[] args) {
+        int a=9;
+        int b=10;
+        System.out.println(a&b);
         
     }
     
---
+++ 18_BitManuplation/swapXor.java
@@ -0,0 +1,13 @@
+public class swapXor {
+    public static void main(String[] args) {
+        int a=5;
+        int b=6;
+         System.out.println("before Swap a = "+a+"  b = "+b);
+
+         a=a^b;
+         b=a^b;
+         a=a^b;
+         System.out.println("After Swap a = "+a+" b = "+b);
+    }
+    
+}
